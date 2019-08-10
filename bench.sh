2010c78bbbf1ea9d51148107028a3cbd29d48fae
Richard Startin <richard@openkappa.co.uk>
2019-08-10 20:05:58 +0100

    script

--- bench.sh
+++ bench.sh
@@ -7,4 +7,4 @@ export JAVA_HOME=${PANAMA:-/tmp/panama}
 export PATH=$JAVA_HOME/bin:$PATH
 
 mvn clean package
-taskset -c 0 java -jar target/benchmarks.jar -f 1 -wi 5 -w 1 -i 5 -r 1 -rf CSV -rff ${filename} -bm thrpt .*xxhash32
+taskset -c 0 java -jar target/benchmarks.jar -f 1 -wi 5 -w 1 -i 5 -r 1 -rf CSV -rff ${filename} -bm thrpt 
