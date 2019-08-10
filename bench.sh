a03c1f7696b0ec5a438e77b201eb87e1bb832b07
Richard Startin <richard@openkappa.co.uk>
2019-08-10 20:37:34 +0100

    results

--- bench.sh
+++ bench.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-filename=${1:-bench.csv}
+filename=${1:-results.csv}
 
 
 export JAVA_HOME=${PANAMA:-/tmp/panama}
 export PATH=$JAVA_HOME/bin:$PATH
 
 mvn clean package
-taskset -c 0 java -jar target/benchmarks.jar -f 1 -wi 5 -w 1 -i 5 -r 1 -rf CSV -rff ${filename} -bm thrpt 
+taskset -c 0 java -jar target/benchmarks.jar -f 1 -wi 5 -w 1 -i 5 -r 1 -rf CSV -rff ${filename} -bm thrpt
